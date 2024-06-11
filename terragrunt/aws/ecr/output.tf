e08513319aea1fde3285085c34f804281b6e4cbe
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-06-11 22:09:24 +0000

    Changing the names of the outputs

--- terragrunt/aws/ecr/output.tf
+++ terragrunt/aws/ecr/output.tf
@@ -4,10 +4,10 @@
 
 output "ecr_repository_url" {
   description = "URL of the URL shortener ECR"
-  value       = aws_ecr_repository.api.repository_url
+  value       = aws_ecr_repository.doc_to_hml_ecr.repository_url
 }
 
 output "ecr_repository_arn" {
   description = "Arn of the ECR Repository"
-  value       = aws_ecr_repository.api.arn
+  value       = aws_ecr_repository.doc_to_hml_ecr.arn
 }
