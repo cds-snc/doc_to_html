2d3dd4da736b796f85d3097a83632c83ffc05dc3
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-06-11 22:30:26 +0000

    Fixing typos

--- terragrunt/aws/ecr/output.tf
+++ terragrunt/aws/ecr/output.tf
@@ -4,10 +4,10 @@
 
 output "ecr_repository_url" {
   description = "URL of the Doc_to_html ECR"
-  value       = aws_ecr_repository.doc_to_hml_ecr.repository_url
+  value       = aws_ecr_repository.doc_to_html_ecr.repository_url
 }
 
 output "ecr_repository_arn" {
   description = "Arn of the Doc_to_html ECR Repository"
-  value       = aws_ecr_repository.doc_to_hml_ecr.arn
+  value       = aws_ecr_repository.doc_to_html_ecr.arn
 }
